0122385cef73082a6c8a71b754a99d5f181a107f
Chris Abiva <chrisabiva@hotmail.com>
2021-07-31 20:06:47 -0700

    Initializing ServiceWorker

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 019-WebPerformance
+
+Install Webpack
+`npm install webpack webpack-cli -D`
+`babel-loader`
+`npm install --save-dev @babel/preset-env`
---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/js/index.js",
+]
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
