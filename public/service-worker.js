1bca554329ce02766c1a7043bd6214c70a5a8f10
Chris Abiva <chrisabiva@hotmail.com>
2021-08-01 21:11:43 -0700

    Added IndexDB.js. Offline IndexDB is working, But there console.log errors. Need to be updated.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,41 @@
-const { cache } = require("webpack");
-
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "/js/index.js",
-]
+  './',
+  './index.html',
+  './css/style.css',
+  './js/indexedDB.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './js/index.js',
+  './styles.css',
+  './dist/bundle.js',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
-      .open(STATIC_CACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
-// Cleaning the old caches
-self.addEventListener("activate", event => {
-  const currentCaches = [STATIC_CACHES, RUNTIME_CACHE];
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
       })
-      .then(cachesToDelete => {
+      .then((cachesToDelete) => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete)
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
           })
         );
       })
@@ -43,12 +43,36 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  // GET Request are not cached and requested to other origins are not cahced.
-  if ( event.request.method !== "GET" ||
-  !event.request.url.startsWith(self.location.origin)) 
-  {
-    event.respondWith(fetch(event.request));
-  return;  
-}
-})
+
+
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(RUNTIME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(PRECACHE).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
       "theme-color": "#ffffff",
       start_url: "/",
       icons: [
-        {
+        { 
           src: path.resolve("./public/icons/icon-192x192.png"),
           sizes: [96, 128, 192, 256, 384, 512],
           destination: path.join("assets", "icons")
