106ce49636e6bad9c2338205e746c6b78ee22982
Chris Abiva <chrisabiva@hotmail.com>
2021-08-02 18:59:54 -0700

    Fixed bugs on service worker and indexedDB errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,12 @@
 const FILES_TO_CACHE = [
-  './',
   './index.html',
   './css/style.css',
   './js/indexedDB.js',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './js/index.js',
-  './styles.css',
-  './dist/bundle.js',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  './manifest.json',
 ];
 
 const PRECACHE = 'precache-v1';
